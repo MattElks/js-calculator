b4b5b66874df354b43d9d1ae38c97e66547f0175
matt_elks <mattelksdeveloper@gmail.com>
2022-08-11 14:39:36 +0100

    p tags removed, CSS modified, unnecessary variables removed

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-/* 
-> Add event listeners to each button
-> Add button to string
-> String => do math
-> return result
-*/
-
 //symbols
 const result = document.querySelector(".btn-1");
 const cancel = document.querySelector(".btn-2");
-const negPos = document.querySelector("btn-3");
+const negPos = document.querySelector(".btn-3");
 const divByHun = document.querySelector(".btn-4");
 const divide = document.querySelector(".btn-5");
 const multiply = document.querySelector(".btn-9");
@@ -16,14 +9,18 @@ const minus = document.querySelector(".btn-13");
 const add = document.querySelector(".btn-17");
 const decimal = document.querySelector(".btn-19");
 const equals = document.querySelector(".btn-20");
-//numbers
-const zero = document.querySelector(".btn-18");
-const one = document.querySelector(".btn-14");
-const two = document.querySelector(".btn-15");
-const three = document.querySelector(".btn-16");
-const four = document.querySelector(".btn-10");
-const five = document.querySelector(".btn-11");
-const six = document.querySelector(".btn-12");
-const seven = document.querySelector(".btn-6");
-const eight = document.querySelector(".btn-7");
-const nine = document.querySelector(".btn-8");
+
+const addInfo = (button) => {
+  result.innerHTML === "0"
+    ? (result.innerHTML = button.innerHTML)
+    : (result.innerHTML += button.innerHTML);
+  console.log(button.innerHTML);
+  console.log(result.innerHTML);
+};
+
+const button = document.querySelectorAll(".btn");
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    addInfo(button);
+  });
+});

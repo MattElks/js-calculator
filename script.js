0bf612018a56cc0fbb3e3a6fce7ee2c6af831dc2
matt_elks <mattelksdeveloper@gmail.com>
2022-09-08 10:51:53 +0100

    query selectors removed, calculator object added

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-//symbols
-const result = document.querySelector(".btn-1");
-const cancel = document.querySelector(".btn-2");
-const negPos = document.querySelector(".btn-3");
-const divByHun = document.querySelector(".btn-4");
-const divide = document.querySelector(".btn-5");
-const multiply = document.querySelector(".btn-9");
-const minus = document.querySelector(".btn-13");
-const add = document.querySelector(".btn-17");
-const decimal = document.querySelector(".btn-19");
-const equals = document.querySelector(".btn-20");
-
-const addInfo = (button) => {
-  result.innerHTML === "0"
-    ? (result.innerHTML = button.innerHTML)
-    : (result.innerHTML += button.innerHTML);
-  console.log(button.innerHTML);
-  console.log(result.innerHTML);
+//handles values
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
 };
-
-const button = document.querySelectorAll(".btn");
-button.forEach((button) => {
-  button.addEventListener("click", () => {
-    addInfo(button);
-  });
-});

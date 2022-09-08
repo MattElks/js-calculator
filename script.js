3f3233beec650c452b3540763dfe573378ac0506
matt_elks <mattelksdeveloper@gmail.com>
2022-09-08 11:14:30 +0100

    keys pressed function added

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ const updateDisplay = () => {
   display.innerHTML = calculator.displayValue;
 };
 updateDisplay();
+
+//handles keys pressed
+const keys = document.querySelector("main");
+keys.addEventListener("click", (event) => {
+  const { target } = event;
+  if (!target.matches("div") || target.classList.contains("display")) {
+    return;
+  }
+  if (target.classList.contains("operator")) {
+    console.log("operator: ", target.innerHTML);
+    return;
+  }
+  if (target.classList.contains("decimal")) {
+    console.log("decimal: ", target.innerHTML);
+    return;
+  }
+  if (target.classList.contains("clear")) {
+    console.log("clear: ", target.innerHTML);
+    return;
+  }
+  console.log("digit: ", target.innerHTML);
+  console.log(event);
+});
